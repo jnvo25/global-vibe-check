800c112b38372c5cf02b2d5e31e85cc426e2581a
Jonathan Vo <jonatnv1@uci.edu>
2021-01-29 22:57:01 -0800

    Connect links in navbar and adjust colors

--- src/screens/NavBar.jsx
+++ src/screens/NavBar.jsx
@@ -1,33 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Button, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 export default function NavBar() {
 
+    // TODO: Open dropdown on hover
+    // TODO: Link links
+
+    // const [isOpen, setOpen] = useState(false);
+
     return(
-        <Navbar sticky="top" variant="light" expand="lg" className="bottom-nav pb-0">
+        <Navbar sticky="top" variant="dark" expand="lg" className="bottom-nav pb-0">
             <LinkContainer to="/" exact>
                 <Navbar.Brand>
-                    <img className="brand" src={""} alt="Hero" />
+                    <img className="brand" 
+                        // open={isOpen}
+                        style={{width: "100px"}} 
+                        src={"https://firebasestorage.googleapis.com/v0/b/global-vibe-check.appspot.com/o/images%2Ftransparent-global-logo.png?alt=media&token=294cb8c8-ebf9-492f-a397-109ea4325d01"} 
+                        alt="Hero" />
                 </Navbar.Brand>
             </LinkContainer>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <NavDropdown title="DropDown">
-                        <LinkContainer>
+                <Nav className="mr-auto">
+                    <NavDropdown title="Menu" open={true}>
+                        <LinkContainer to="/about">
                             <NavDropdown.Item>About Us</NavDropdown.Item>
                         </LinkContainer>
-                        <LinkContainer>
+                        <LinkContainer to="/guidelines">
                             <NavDropdown.Item>Covid Guidelines</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to="/stats">
+                            <NavDropdown.Item>Covid Statistics</NavDropdown.Item>
                         </LinkContainer>
                         {/* <NavDropdown.Divider />
                         <NavDropdown.Item>Separated link</NavDropdown.Item> */}
                     </NavDropdown>
-                    <LinkContainer to="/contactus">
+                    <LinkContainer to="/globe">
                         <Nav.Link>Interactive Global Map</Nav.Link>
                     </LinkContainer>
-                    <Button className="nav-button" variant="secondary" href="tel:818-993-1369"><i className="fas fa-phone-alt pr-1"/>Call Us</Button>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
